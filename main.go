d8cbcbcb0644a9148fa73b54896fd944a9fc649e
unknown <rizal.bahriawan@pusilkom.com>
2025-09-05 22:15:46 +0700

    add input param

--- main.go
+++ main.go
@@ -19,6 +19,14 @@ import (
 	"github.com/xuri/excelize/v2"
 )
 
+type ExportParam struct {
+	projectFilter       string
+	fileName            string
+	isRandomizeDuration bool
+	minDuration         int
+	maxDuration         int
+}
+
 func login(baseURL, username, password string) (*response.LoginResponse, error) {
 	loginReq := request.LoginRequest{
 		Username: username,
@@ -47,7 +55,7 @@ func login(baseURL, username, password string) (*response.LoginResponse, error) 
 
 // Fetch log activity
 func fetchLogActivity(baseURL, token string, idEmployee int, month int, year int) (*response.LogActResponse, error) {
-	// Build query string
+
 	url := baseURL + "/log-act-detail-non-aj/table?sort=date|asc&idEmployee=" + strconv.Itoa(idEmployee) + "&months=" + strconv.Itoa(month) + "&years=" + strconv.Itoa(year)
 
 	// Create request
@@ -73,8 +81,13 @@ func fetchLogActivity(baseURL, token string, idEmployee int, month int, year int
 	return &logResp, nil
 }
 
-func exportToExcel(activities []response.Activity, projectFilter string, fileName string) error {
-	// Create new Excel file
+func exportToExcel(activities []response.Activity, param ExportParam) error {
+	projectFilter := param.projectFilter
+	fileName := param.fileName + ".xlsx"
+	isRandomizeDuration := param.isRandomizeDuration
+	minDuration := param.minDuration
+	maxDuration := param.maxDuration
+
 	f := excelize.NewFile()
 	sheet := "Sheet1"
 	index, err := f.NewSheet(sheet)
@@ -82,8 +95,7 @@ func exportToExcel(activities []response.Activity, projectFilter string, fileNam
 		return err
 	}
 
-	// Parse all activities into grouped map by year-month
-	layout := "02-01-2006" // your date format: dd-mm-yyyy
+	layout := "02-01-2006"
 	grouped := make(map[string][]response.Activity)
 
 	for _, a := range activities {
@@ -94,11 +106,10 @@ func exportToExcel(activities []response.Activity, projectFilter string, fileNam
 		if err != nil {
 			return fmt.Errorf("invalid date format: %s", a.DateString)
 		}
-		key := t.Format("2006-01") // yyyy-mm
+		key := t.Format("2006-01")
 		grouped[key] = append(grouped[key], a)
 	}
 
-	// Sort keys (months)
 	var keys []string
 	for k := range grouped {
 		keys = append(keys, k)
@@ -107,11 +118,9 @@ func exportToExcel(activities []response.Activity, projectFilter string, fileNam
 
 	row := 1
 	for _, k := range keys {
-		// Parse month name
 		t, _ := time.Parse("2006-01", k)
 		monthName := t.Format("January 2006")
 
-		// Convert to Indonesian month name
 		indonesianMonths := map[time.Month]string{
 			time.January:   "Januari",
 			time.February:  "Februari",
@@ -128,16 +137,13 @@ func exportToExcel(activities []response.Activity, projectFilter string, fileNam
 		}
 		monthName = indonesianMonths[t.Month()]
 
-		// Empty row before each month (except the first)
 		if row > 1 {
 			row++
 		}
 
-		// Month name row
 		f.SetCellValue(sheet, fmt.Sprintf("A%d", row), monthName)
 		row++
 
-		// Header row
 		headers := []string{"Tanggal", "Durasi (Jam)", "Kegiatan"}
 		for col, h := range headers {
 			cell, _ := excelize.CoordinatesToCellName(col+1, row)
@@ -145,28 +151,24 @@ func exportToExcel(activities []response.Activity, projectFilter string, fileNam
 		}
 		row++
 
-		// Sort activities inside the month by date
 		acts := grouped[k]
-		// sort.Slice(acts, func(i, j int) bool {
-		// 	ti, _ := time.Parse(layout, acts[i].DateString)
-		// 	tj, _ := time.Parse(layout, acts[j].DateString)
-		// 	return ti.Before(tj)
-		// })
 
-		// Write rows
 		for _, a := range acts {
 			f.SetCellValue(sheet, fmt.Sprintf("A%d", row), a.DateString)
-			randomNumber := rand.Intn(a.Duration-3) + 4
-			f.SetCellValue(sheet, fmt.Sprintf("B%d", row), randomNumber)
+			duration := a.Duration
+			if isRandomizeDuration {
+				randomNumber := rand.Intn(maxDuration-minDuration+1) + minDuration
+				duration = randomNumber
+			}
+
+			f.SetCellValue(sheet, fmt.Sprintf("B%d", row), duration)
 			f.SetCellValue(sheet, fmt.Sprintf("C%d", row), a.ActivityDetail)
 			row++
 		}
 	}
 
-	// Set active sheet
 	f.SetActiveSheet(index)
 
-	// Save file
 	if err := f.SaveAs(fileName); err != nil {
 		return err
 	}
@@ -181,9 +183,25 @@ func main() {
 	baseURL := os.Getenv("BASE_URL")
 	username := os.Getenv("USERNAME_ESS")
 	password := os.Getenv("PASSWORD_ESS")
-	fmt.Println("baseUrl", baseURL)
-	fmt.Println("username", username)
-	fmt.Println("password", password)
+	monthsExported := os.Getenv("MONTHS_EXPORTED")
+	var months []int
+	if err := json.Unmarshal([]byte(monthsExported), &months); err != nil {
+		log.Fatal(err)
+	}
+	year, err := strconv.Atoi(os.Getenv("YEAR_EXPORTED"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	projectName := os.Getenv("PROJECT_NAME_EXPORTED")
+	resultFileName := os.Getenv("RESULT_FILE_NAME")
+	randomizeDuration, err := strconv.ParseBool(os.Getenv("RANDOMIZE_DURATION"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	minDuration, err := strconv.Atoi(os.Getenv("MIN_DURATION"))
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	// Step 1: Login
 	loginResp, err := login(baseURL, username, password)
@@ -193,26 +211,41 @@ func main() {
 	fmt.Println("Login successful, token:", loginResp.IdToken)
 	fmt.Printf("User: %+v\n", loginResp.UserInfo)
 
-	// month := 7
-	months := []int{7, 8, 9}
-	year := 2025
-
 	var acts []response.Activity
 	for _, month := range months {
 		logResp, err := fetchLogActivity(baseURL, loginResp.IdToken, loginResp.UserInfo.EmployeeID, month, year)
 		if err != nil {
 			log.Fatal("Fetch error:", err)
 		}
+
 		acts = append(acts, logResp.Data...)
 	}
 
+	maxDuration := 0
 	for _, a := range acts {
+		maxDuration = a.Duration
 		fmt.Printf("Tanggal: %s, Project: %s, Duration: %d, Activity: %s\n",
 			a.DateString, a.ProjectName, a.Duration, a.ActivityDetail)
 	}
+	envMaxDuration := os.Getenv("MAX_DURATION")
+	if envMaxDuration != "" {
 
-	if err := exportToExcel(acts, "DTD UI - SLCM", "timesheet.xlsx"); err != nil {
+		maxDuration, err = strconv.Atoi(envMaxDuration)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	fmt.Println("maxdur", maxDuration)
+	exportParam := ExportParam{
+		projectFilter:       projectName,
+		fileName:            resultFileName,
+		isRandomizeDuration: randomizeDuration,
+		minDuration:         minDuration,
+		maxDuration:         maxDuration,
+	}
+
+	if err := exportToExcel(acts, exportParam); err != nil {
 		log.Fatal("Excel export error:", err)
 	}
-	fmt.Println("Excel file created: timesheet.xlsx")
+	fmt.Println("Excel file created: " + resultFileName + ".xlsx")
 }
